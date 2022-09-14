70099b5e1135f1c1258dc606c278f56d2de4c4c6
ajitrchv <67496650+ajitrchv@users.noreply.github.com>
2022-09-14 21:56:02 +0530

    added classbased view for forms:

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import HomeView, ThanksView
+from .views import HomeView, ThanksView, ContactFormView
 
 
 app_name = 'classroom'
 
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
-    path('thankyou/', ThanksView.as_view(), name="thanks")
+    path('thankyou/', ThanksView.as_view(), name="thanks"),
+    path('contact/', ContactFormView.as_view(),name='contact'),
+    
 ]
--- classroom/views.py
+++ classroom/views.py
@@ -1,6 +1,7 @@
+from operator import contains
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, FormView
+from classroom.forms import ContactForm
 # Create your views here.
 # def home(request):
 #     return render(request,'classroom/home.html')
@@ -10,4 +11,15 @@ class HomeView(TemplateView):
     
 class ThanksView(TemplateView):
     template_name= 'classroom/thankyou.html'
+    
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name='classroom/contact.html'
+    
+    success_url = "/classroom/thankyou/"
+    
+    def form_valid(self,form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+       
     
